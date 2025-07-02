9bbbdd719a6666716567170822bd3e35e9929285
Hung Nguyen <nthung2112@gmail.com>
2025-07-02 15:47:27 +0700

    Add common route

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
-import * as schema from "./schema/index.js";
+import * as schema from "./schema";
 import "dotenv/config";
 
 // get database url from env, get database
